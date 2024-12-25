1216a8dd3d23bac33ba894d1de97e54ed5b7dddb
1992Arun <arunkumar.endiyur@gmail.com>
2024-12-25 16:11:05 +0530

    new code

---
+++ AccessModifier/src/differentPackage/org/Demo4.java
@@ -0,0 +1,20 @@
+package differentPackage.org;
+
+import samepackage.org.Demo1;
+
+public class Demo4  {
+	
+	public static void main(String[] args) {
+		
+		
+		Demo4 obj7 = new Demo4();
+	
+
+	Demo1 obj6 = new Demo1();
+	
+	  System.out.println(obj6.x1);
+//	System.out.println(obj6.x2);
+//	System.out.println(obj6.x3);
+//	System.out.println(obj6.x4);
+}
+}
---
+++ AccessModifier/src/samepackage/org/Demo3.java
@@ -0,0 +1,18 @@
+package samepackage.org;
+
+public class Demo3 extends Demo1 {
+	
+	public static void main(String[] args) {
+		
+		
+	
+	
+	Demo1 obj3= new Demo1();
+	
+	System.out.println(obj3.x1);
+//	System.out.println(obj3.x2);
+	System.out.println(obj3.x3);
+	System.out.println(obj3.x4);
+
+}
+}
